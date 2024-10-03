04e428af1d5c2464da3e0fea6442a055fd7d12e4
Marie <khandanyanm@gmail.com>
2024-10-03 16:22:32 +0300

    - Правки

--- src/view/profile/components/InformationContent.tsx
+++ src/view/profile/components/InformationContent.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
+import { FlexDir } from "@/styled-components/styles";
 
 interface InformationContentProps {}
 
 export const InformationContent: FC<InformationContentProps> = () => {
   return (
     <>
-      <Text>Данные профиля</Text>
+      <FlexDir>Данные профиля</FlexDir>
     </>
   );
 };
